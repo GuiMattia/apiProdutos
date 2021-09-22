98d0424fc508788d8c3e4741d9f46014a9f91848
Guilherme <guilherme-mattia123@hotmail.com>
2021-09-21 21:15:30 -0300

    Classe produto sendo criada

---
+++ src/main/java/br/senac/devweb/api/product/produto/ProdutoRepresentation.java
@@ -0,0 +1,6 @@
+package br.senac.devweb.api.product.produto;
+
+public class ProdutoRepresentation {
+    public class CreateOrUpdateProduto {
+    }
+}
---
+++ src/main/java/br/senac/devweb/api/product/produto/ProdutoService.java
@@ -0,0 +1,43 @@
+package br.senac.devweb.api.product.produto;
+
+import br.senac.devweb.api.product.exceptions.NotFoundException;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class ProdutoService {
+
+
+    private final ProdutoRepository produtoRepository;
+
+    public Produto salvar() {
+        return this.produtoRepository.save(new Produto());
+    }
+
+    public void atualizar(Long id) {
+        Produto produto = this.buscarUm(id);
+
+    }
+
+    public List<Produto> buscarTodos(Predicate filter) {
+        return this.produtoRepository.findAll(filter);
+    }
+    public Produto buscarUm(Long id) {
+        BooleanExpression filter = QProduto.produto.id.eq(id)
+                .and(QProduto.produto.status.eq(Produto.Status.ATIVO));
+        return this.produtoRepository.findOne(filter).orElseThrow(() -> new NotFoundException("Produto não encontrado"));
+    }
+
+    public void deleteProduto(Long id) {
+        Produto produto = this.buscarUm(id);
+        produto.setStatus(Produto.Status.INATIVO);
+        this.produtoRepository.save(produto);
+    }
+
+}
+
